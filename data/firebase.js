9bd88aea22fa38dbca8d19822e2927e9363c8c10
FFT_dareka <152751544+FFTdareka@users.noreply.github.com>
2026-07-29 02:03:45 +0000

    Update files

--- data/firebase.js
+++ data/firebase.js
@@ -1,9 +1,11 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-analytics.js";
 import { getAuth, onAuthStateChanged, GoogleAuthProvider, signInWithPopup, signOut } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
+import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
 
 let app,
-    auth;
+    auth,
+    db;
 
 fetch('data/staData.json')
     .then(res => res.json())
@@ -11,11 +13,13 @@ fetch('data/staData.json')
         app = initializeApp(f.firebase);
         getAnalytics(app);
         auth = getAuth(app);
+        db = getFirestore(app);
         status();
     });
 
 function status() {
     let userStatus = document.getElementById("userStatus");
+    let userName = document.getElementById("userName");
     let loginBtn = document.getElementById("login");
     onAuthStateChanged(auth, user => {
         if (user) {
@@ -25,6 +29,7 @@ function status() {
             loginBtn.addEventListener("click", userLogout);
         } else {
             userStatus.innerText = "未ログイン";
+            userName.innerText = "";
             loginBtn.innerText = "ログイン";
             loginBtn.removeEventListener("click", userLogout);
             loginBtn.addEventListener("click", userLogin);
@@ -35,8 +40,11 @@ function status() {
 function userLogin() {
     let provider = new GoogleAuthProvider();
     signInWithPopup(auth, provider)
-        .then(result => console.log("ログイン成功:", result.user))
-        .catch(error => console.error("ログイン失敗:", error.code, error.message));
+        .then(res => {
+            console.log("ログイン成功:", res.user);
+            loadUserdata(res.user.uid);
+        })
+        .catch(er => console.error("ログイン失敗:", er.code, er.message));
 }
 
 function userLogout() {
@@ -53,4 +61,20 @@ function userLogout() {
         userSpace.appendChild(logoutStatus);
         setTimeout(() => document.getElementById(logoutStatus).innerText = "", 5000);
     });
+}
+
+function loadUserdata(uid) {
+    getDoc(doc(db, "user", uid))
+        .then(snap => {
+            let userName = document.getElementById("userName");
+            if (snap.exists()) {
+                let d = snap.data();
+                console.log("データ:", d);
+                if (d.name) userName.innerText = d.name; else userName.innerText = "匿名";
+            } else {
+                console.log("データが存在しません");
+                userName.innerText = "匿名";
+            }
+        })
+        .catch(er => console.error("読み込み失敗:", er));
 }
