348180d927719c3abda4d4f2606d332dee6d52ed
FFT_dareka <152751544+FFTdareka@users.noreply.github.com>
2026-07-29 02:57:45 +0000

    Update and Create files

--- data/firebase.js
+++ data/firebase.js
@@ -1,11 +1,12 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-analytics.js";
 import { getAuth, onAuthStateChanged, GoogleAuthProvider, signInWithPopup, signOut } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
-import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
+import { getFirestore, doc, getDoc, setDoc } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
 
 let app,
     auth,
-    db;
+    db,
+    uid;
 
 fetch('data/staData.json')
     .then(res => res.json())
@@ -23,17 +24,26 @@ function status() {
     let loginBtn = document.getElementById("login");
     onAuthStateChanged(auth, user => {
         if (user) {
-            userStatus.innerText = "ログイン中";
+            uid = user.uid;
+            userStatus.innerText = " としてログイン中";
             loginBtn.innerText = "ログアウト";
-            loadUserdata(user.uid);
             loginBtn.removeEventListener("click", userLogin);
             loginBtn.addEventListener("click", userLogout);
+            if (location.href == "https://fftdareka.github.io/ototetsu_memory/user.html") {
+                let sSpace = document.getElementById("setting");
+                sSpace.innerHTML = `<div id="nameS">現在の名前: <span id="userNameS"></span><br>新しい名前: <input id="newName" type="text" placeholder="名前を入力"></div><button id="updateBtn" type="button" onclick="updateUser()" disabled>更新</button>`;
+            }
+            loadUserdata(uid);
         } else {
             userStatus.innerText = "未ログイン";
             userName.innerText = "";
             loginBtn.innerText = "ログイン";
             loginBtn.removeEventListener("click", userLogout);
             loginBtn.addEventListener("click", userLogin);
+            if (location.href == "https://fftdareka.github.io/ototetsu_memory/user.html") {
+                let sSpace = document.getElementById("setting");
+                sSpace.innerHTML = `<span>このページはGoogleアカウントにログインした人のみ利用可能です。<br><span id="login2" onclick="userLogin()">ログインはこちら</span></div>`;
+            }
         }
     });
 }
@@ -70,11 +80,40 @@ function loadUserdata(uid) {
             if (snap.exists()) {
                 let d = snap.data();
                 console.log("データ:", d);
-                if (d.name) userName.innerText = d.name; else userName.innerText = "匿名";
+                userName.innerText = d.name || "匿名";
+                if (location.href == "https://fftdareka.github.io/ototetsu_memory/user.html") document.getElementById("userNameS").innerText = d.name || "匿名";
             } else {
                 console.log("データが存在しません");
                 userName.innerText = "匿名";
+                if (location.href == "https://fftdareka.github.io/ototetsu_memory/user.html") document.getElementById("userNameS").innerText = "匿名";
             }
         })
         .catch(er => console.error("読み込み失敗:", er));
+}
+
+function updateUser() {
+    let newNameE = document.getElementById(newName);
+    if(newNameE) {
+        setDoc(doc(db, "user", uid), {
+            name: newNameE.value
+        }, { merge: true })
+        .then(() => {
+            console.log("更新成功");
+            let noticeU = document.createElement("div");
+            noticeU.id = "noticeU";
+            noticeU.classList.add("notice");
+            noticeU.innerText = "更新が完了しました。";
+            document.getElementById("setting").appendChild(noticeU);
+            setTimeout(() => document.getElementById("noticeU").remove(), 5000);
+        })
+        .catch(error => {
+            let noticeU = document.createElement("div");
+            noticeU.id = "noticeU";
+            noticeU.classList.add("notice");
+            noticeU.innerText = "更新に失敗しました。";
+            document.getElementById("setting").appendChild(noticeU);
+            setTimeout(() => document.getElementById("noticeU").remove(), 5000);
+            console.error("保存失敗:", error)
+        });
+    }
 }
