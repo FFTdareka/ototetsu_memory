c6a8b01c2f4cbf3967d0f33dd2a12aed86f9e44b
FFT_dareka <152751544+FFTdareka@users.noreply.github.com>
2026-08-13 15:26:06 +0000

    Update and Create files

--- data/mnews.js
+++ data/mnews.js
@@ -9,6 +9,38 @@ async function setNews() {
     if (data == "ニュースの取得に失敗しました。") {
         newsS.innerText = data;
     } else {
-        
+        data.forEach((news, i) => {
+            if (i > 0) {
+                newsS.appendChild(document.createElement("br"));
+            }
+
+            let g = document.createElement("div");
+            g.classList.add("news");
+            g.id = `news${news.ID}`;
+
+            let t = document.createElement("div");
+            t.innerText = news.title;
+            t.style = "font-weight: bold;";
+            g.appendChild(t);
+
+            let s = document.createElement("div");
+            let txt = news.body.split("\n");
+            txt.forEach((line, j) => {
+                if (j > 0) s.appendChild(document.createElement("br"));
+                let sp = document.createElement("span");
+                sp.innerText = line;
+                s.appendChild(sp);
+            });
+            g.appendChild(s);
+
+            let p = document.createElement("div");
+            p.innerText = `執筆者:${news.author}`;
+            g.appendChild(p);
+            g.addEventListener("click", () => {
+                location.href = `https://fftdareka.github.io/ototetsu_memory/member/editNews.html?id=${news.ID}`;
+            });
+
+            el.appendChild(g);
+        });
     }
 }
